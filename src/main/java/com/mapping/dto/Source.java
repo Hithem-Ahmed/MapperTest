ef2728fef787fbe9efb06a47bc3f176c32705a3f
hahmed <hitham.ahmed@cegedim.com>
2023-09-26 09:28:52 +0300

    Initial commit

---
+++ src/main/java/com/mapping/dto/Source.java
@@ -0,0 +1,59 @@
+package com.mapping.dto;
+
+public class Source {
+
+    private int foo;
+    private Long bar;
+    private int qax;
+    private Long baz;
+    private int zip;
+    private String someType;
+
+    public int getFoo() {
+        return foo;
+    }
+
+    public void setFoo(int foo) {
+        this.foo = foo;
+    }
+
+    public Long getBar() {
+        return bar;
+    }
+
+    public void setBar(Long bar) {
+        this.bar = bar;
+    }
+
+    public int getQax() {
+        return qax;
+    }
+
+    public void setQax(int qax) {
+        this.qax = qax;
+    }
+
+    public Long getBaz() {
+        return baz;
+    }
+
+    public void setBaz(Long baz) {
+        this.baz = baz;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getSomeType() {
+        return someType;
+    }
+
+    public void setSomeType(String someType) {
+        this.someType = someType;
+    }
+}
